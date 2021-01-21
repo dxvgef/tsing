51204eb0b33577ed88b90cdca1d0a40185eeeab3
dxvgef <dxvgef@outlook.com>
2021-01-21 18:58:19 +0800

    add nolint

--- context.go
+++ context.go
@@ -234,7 +234,7 @@ func (ctx *Context) FormParam(key string) (string, bool) {
 	return ctx.Request.Form[key][0], true
 }
 
-// 将body里的json数据反序列化到传入的对象
+// nolint 将body里的json数据反序列化到传入的对象
 func (ctx *Context) UnmarshalJSON(obj interface{}) error {
 	body, err := ioutil.ReadAll(ctx.Request.Body)
 	if err != nil {
