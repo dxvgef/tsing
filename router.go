37560b4975f7c6aa933d211f3adfba7dc5cb0064
dxvgef <dxvgef@outlook.com>
2024-04-16 13:55:07 +0800

    加入Static()方法实现静态目录

--- router.go
+++ router.go
@@ -1,7 +1,11 @@
 package tsing
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"path"
+	"path/filepath"
 	"strings"
 )
 
@@ -102,20 +106,73 @@ func (group *RouterGroup) Match(methods []string, relativePath string, handlers 
 	}
 }
 
+// Static 注册一个指向服务端本地目录的静态路由，例如：
+// Static("/public", "./public")
+func (group *RouterGroup) Static(relativePath, localPath string, listDir bool) {
+	// 清理路径
+	localPath = filepath.Clean(localPath)
+	// 禁止路径参数
+	if strings.Contains(relativePath, ":") || strings.Contains(relativePath, "*") {
+		panic("relativePath for this route cannot use ':' and '*'")
+	}
+	// 本地路径不能为空
+	if localPath == "" {
+		panic("localPath cannot be empty")
+	}
+
+	// 创建路由处理器
+	handler := func(ctx *Context) error {
+		relPath := ctx.PathValue("filepath")
+		finalAbsPath := path.Join(localPath, relPath)
+		fileInfo, err := os.Stat(finalAbsPath)
+		if err != nil {
+			ctx.Status = http.StatusNotFound
+			ctx.Error = fmt.Errorf("%v", http.StatusText(http.StatusNotFound))
+			if group.engine.config.ErrorHandler != nil {
+				group.engine.config.ErrorHandler(ctx)
+			} else {
+				ctx.ResponseWriter.WriteHeader(ctx.Status)
+				_, _ = ctx.ResponseWriter.Write(strToBytes(ctx.Error.Error())) //nolint:errcheck
+			}
+			return nil
+		}
+		// 禁止列出目录
+		if fileInfo.IsDir() && !listDir {
+			ctx.Status = http.StatusForbidden
+			ctx.Error = fmt.Errorf("%v", http.StatusText(http.StatusForbidden))
+			if group.engine.config.ErrorHandler != nil {
+				group.engine.config.ErrorHandler(ctx)
+			} else {
+				ctx.ResponseWriter.WriteHeader(ctx.Status)
+				_, _ = ctx.ResponseWriter.Write(strToBytes(ctx.Error.Error())) //nolint:errcheck
+			}
+			return nil
+		}
+		http.ServeFile(ctx.ResponseWriter, ctx.Request, finalAbsPath)
+		return nil
+	}
+
+	// 加入内部路径参数
+	finalURLPath := path.Join(relativePath, "/*filepath")
+	// 注册GET和HEAD方法的路由
+	group.GET(finalURLPath, handler)
+	group.HEAD(finalURLPath, handler)
+}
+
 // StaticFile 注册一个指向服务端本地文件的静态路由，例如：
 // StaticFile("favicon.ico", "./resources/favicon.ico")
-func (group *RouterGroup) StaticFile(relativePath, filepath string) {
-	group.staticFileHandler(relativePath, func(c *Context) error {
-		c.ServeFile(filepath)
+func (group *RouterGroup) StaticFile(relativePath, filePath string) {
+	group.staticFileHandler(relativePath, func(ctx *Context) error {
+		ctx.ServeFile(filePath)
 		return nil
 	})
 }
 
 // StaticFileFS 与StaticFile函数类型，但可以自定义文件系统，例如：
 // StaticFileFS("favicon.ico", "./resources/favicon.ico", Dir{".", false})
-func (group *RouterGroup) StaticFileFS(relativePath, filepath string, fs http.FileSystem) {
-	group.staticFileHandler(relativePath, func(c *Context) error {
-		c.FileFromFS(filepath, fs)
+func (group *RouterGroup) StaticFileFS(relativePath, filePath string, fs http.FileSystem) {
+	group.staticFileHandler(relativePath, func(ctx *Context) error {
+		ctx.FileFromFS(filePath, fs)
 		return nil
 	})
 }
